124dc73dde65cd64f8b4d55951fef67c219c8022
Arysun <123336853+Arysun@users.noreply.github.com>
2023-01-22 20:43:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const botaoTrailer  = document.querySelector('.botao-trailer');
+const video = document.getElementById('video');
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle('aberto');
+}
+
+botaoTrailer.addEventListener('click', () => {
+    alternarModal();
+    video.setAttribute('src', linkDoVideo)
+})
+
+const modal = document.querySelector('.modal');
+const botaoFecharModal = document.querySelector('.fechar-modal');
+
+
+botaoFecharModal.addEventListener('click', () => {
+    alternarModal();
+    video.setAttribute('src','');
+});
